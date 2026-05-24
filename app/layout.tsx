f121c83c8d3a37ebc00b0b75683c8b4d1de196d3
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-05-24 18:26:14 +0530

    created user route and db setup!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import type { Metadata } from "next";
+import Provider from "./provider";
 
 export const metadata: Metadata = {
   title: "Next.js Premium Startup Boilerplate",
-  description: "Created using the ultimate interactive Next.js stack generator CLI.",
+  description:
+    "Created using the ultimate interactive Next.js stack generator CLI.",
 };
 
 export default function RootLayout({
@@ -16,7 +18,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body style={{ margin: 0, padding: 0 }}>
-          {children}
+          <Provider>{children}</Provider>
         </body>
       </html>
     </ClerkProvider>
--- db/schema.ts
+++ db/schema.ts
@@ -7,12 +7,5 @@ export const users = pgTable("users", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
-export const posts = pgTable("posts", {
-  id: serial("id").primaryKey(),
-  title: text("title").notNull(),
-  content: text("content"),
-  authorId: serial("author_id").references(() => users.id),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-});
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;

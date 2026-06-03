2c6f52d25b9818055d92d14ae6bb3d7ccbf06095
Shivam Gupta <kapilmohangupta20@gmail.com>
2026-06-03 22:02:04 +0530

    workspace buildup and github setup!

--- components/custom/WorkspaceBody.tsx
+++ components/custom/WorkspaceBody.tsx
@@ -2,8 +2,15 @@
 import { UserContextDetail } from "@/app/context/UserContextDetails";
 import Image from "next/image";
 import React, { useContext } from "react";
+import { Button } from "../ui/button";
+import { Link } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const WorkspaceBody = () => {
+  const router = useRouter();
+  const handleRepoAddition = () =>{
+    router.push('/api/github');
+  }
   const context = useContext(UserContextDetail);
   const userDetails = context?.userDetails;
   return (
@@ -14,16 +21,29 @@ const WorkspaceBody = () => {
           Remaining Credits: {userDetails?.credits ?? "Loading..."}
         </div>
       </div>
-      <div className="mt-10 border flex justify-between items-center px-4">
+      <div className="mt-10 border py-2 rounded-2xl flex justify-between items-center px-4">
         <div className="flex items-center space-x-3">
           <div className="bg-white rounded-md p-1">
             <Image src="/image.png" alt="githubLogo" width={70} height={70} />
           </div>
-          <div className="text-2xl mx-4">
-            Connect Github & Add Repo!
-          </div>
+          <div className="text-2xl mx-4">Connect Github & Add Repo!</div>
         </div>
-        <div className="border p-1 cursor-pointer px-2 bg-taupe-900">+ Add</div>
+        <Button onClick={handleRepoAddition} className="cursor-pointer">
+          + Add
+        </Button>
+      </div>
+      <div className="border py-12 mt-10 px-4 flex justify-center flex-col rounded-2xl items-center">
+        <div className="text-6xl">📁</div>
+        <div className="my-5 text-2xl font-semibold">
+          No Repository Connected!
+        </div>
+        <p className="text-orange-200">
+          Connect your Github account and add a repository to generate and run
+          test cases.
+        </p>
+        <Button className="mt-6 cursor-pointer">
+          <Link /> Connect Repo
+        </Button>
       </div>
     </div>
   );
--- components/custom/WorkspaceHeader.tsx
+++ components/custom/WorkspaceHeader.tsx
@@ -7,14 +7,16 @@ const WorkspaceHeader = () => {
     <div className='text-taupe-400 flex justify-between mx-5 py-2 items-center'>
       <div className='text-2xl flex text-purple-300 justify-center items-center'>
         <Image src="/GitProbeLogo.png" className='rounded-md' alt='logo' width={70} height={70} />
-        <div className='mx-1 text-xl'>GitProbe AI</div>
+        <div className='mx-1 text-xl font-bold'>GitProbe AI</div>
       </div>
-      <ul className='text-2xl space-x-16 flex tracking-wider'>
+      <ul className='text-2xl space-x-8 flex tracking-wider'>
         <li className='cursor-pointer'>WorkSpace</li>
         <li className='cursor-pointer'>Pricing</li>
         <li className='cursor-pointer'>Support</li>
       </ul>
+      <div className='flex min-w-[10%] justify-between items-center flex-row-reverse'>
       <UserButton/>
+      </div>
     </div>
   )
 }
